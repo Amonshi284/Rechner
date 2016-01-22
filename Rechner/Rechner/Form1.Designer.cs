f18f99a3878e1528f767a405b6d713173d6dd6c0
Robin Hermann <iah42hermann@gso-koeln.de>
2016-01-22 12:29:17 +0100

    test

--- Rechner/Rechner/Form1.Designer.cs
+++ Rechner/Rechner/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(146, 135);
+            this.button1.Location = new System.Drawing.Point(147, 61);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
