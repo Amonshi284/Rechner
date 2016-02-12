0fe97c7f4aa2b86fa1714062074f1476dd91fcdd
Tobias Marcus <tobtob.marcuc@web.de>
2016-02-12 11:55:33 +0100

    Neue Textbox

--- Rechner/Rechner/Form1.Designer.cs
+++ Rechner/Rechner/Form1.Designer.cs
@@ -46,6 +46,7 @@
             this.button16 = new System.Windows.Forms.Button();
             this.button17 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -209,11 +210,19 @@
             this.textBox1.Size = new System.Drawing.Size(106, 20);
             this.textBox1.TabIndex = 17;
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(42, 24);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(106, 20);
+            this.textBox2.TabIndex = 18;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button17);
             this.Controls.Add(this.button16);
@@ -259,6 +268,7 @@
         private System.Windows.Forms.Button button16;
         private System.Windows.Forms.Button button17;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
